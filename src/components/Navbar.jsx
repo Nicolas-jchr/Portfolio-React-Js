f396d73b488cda747130912ddbbaf9dab5d3308a
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-07-20 15:58:52 +0200

    Style for navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,29 @@
 import { NavLink } from "react-router-dom";
 
-
 function Navbar() {
-    
     return (
-        <>
-            <header>
-                <nav>
-                <li>
-                <NavLink to="/">
-                Accueil
-                </NavLink>
-                </li>
-                <li><NavLink to="/projets">
-                Mes projets
-                </NavLink>
-                </li>
-                <li><NavLink to="/a_propos">
-                A Propos
-                </NavLink></li>
-                </nav>
-            </header>
-        </>
-    )
+        <header className="navbar-header">
+            <nav className="navbar">
+                <ul>
+                    <li>
+                        <NavLink exact to="/" activeClassName="active">
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/projets" activeClassName="active">
+                            Mes projets
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/a_propos" activeClassName="active">
+                            A Propos
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Navbar;
