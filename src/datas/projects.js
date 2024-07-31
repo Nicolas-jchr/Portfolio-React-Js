cc1dddcbfe810dbb8216ffc98a31f13fbd5df12c
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-07-31 17:35:51 +0200

    Add more datas and update page projects

--- src/datas/projects.js
+++ src/datas/projects.js
@@ -1,10 +1,23 @@
-import Project1 from "../assets/projet1.svg"
+import LogoCookieClicker from "../assets/projet1.svg"
+import LogoF1Bet from "../assets/projet2.svg"
+import LogoProtojam from "../assets/protojam.svg"
 
 const infoProjects = [
     {
-      image: Project1,
+      image: LogoCookieClicker,
       titre: "Cookie Clicker",
-    }
+      description: "Site de cookie clicker"
+    },
+    {
+      image: LogoF1Bet,
+      titre: "F1 Bet",
+      description: "Site de paris pour F1"
+    },
+    {
+    image: LogoProtojam,
+    titre: "F1 Bet",
+    description: "Site de paris pour F1"
+  },
   ];
 
   export default infoProjects;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,33 @@
-function Projects() {
-    
+import infoProjects from "../datas/projects";
+import PropTypes from "prop-types";
 
-    return (
-        <>
-            <h1></h1>
-        </>
-    )
+const CardsProjects = ({ image, titre }) => {
+  return (
+    <div>
+      <img src={image} alt={`Logo du projet ${titre}`} />
+      <h3>{titre}</h3>
+    </div>
+  );
+};
+
+CardsProjects.propTypes = {
+    image: PropTypes.string.isRequired,
+    titre: PropTypes.string.isRequired,
+  };
+
+function Projects() {
+  return (
+    <article>
+      <h2>Mes projets réalisés</h2>
+      {infoProjects.map((project, index) => (
+        <CardsProjects 
+          key={index}
+          image={project.image}
+          titre={project.titre}
+        />
+      ))}
+    </article>
+  );
 }
 
 export default Projects;
