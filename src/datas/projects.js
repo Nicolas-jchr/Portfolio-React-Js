f182cc0802e4d2a14215347e5f8d8f63a5433a79
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-07-31 16:56:47 +0200

    Create data for projects

---
+++ src/datas/projects.js
@@ -0,0 +1,13 @@
+
+
+const infoProjects = [
+    {
+    //   image: Meg,
+      name: "Mégane AUTHEMAYOU",
+      role: "Développeuse Web Fullstack",
+      github: "https://github.com/megmayou",
+      linkedin: "https://www.linkedin.com/in/m%C3%A9gane-authemayou/"
+    }
+  ];
+
+  export default infoProjects;
