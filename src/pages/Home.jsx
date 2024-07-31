20af221383a0c6dae908142928c0e0ff13c7078f
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-07-31 16:17:00 +0200

    Header done, start main section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ function Home() {
     return (
         <>
         <header>
+            <img src={""} alt="avatar nico J" />
             <h1>Nicolas JUCHEREAU</h1>
             <h2>Développeur Web / Web mobile FULLSTACK junior</h2>
             <h3>Actuellement à la recherche d'une alternance pour septembre ou novembre 2024</h3>
@@ -12,6 +13,16 @@ function Home() {
                 <article>Logo linkedin</article>
             </section>
         </header>
+        <main>
+            <h2>
+                Mes projets réalisés
+            </h2>
+            <section>
+                <article>
+                    
+                </article>
+            </section>
+        </main>
         </>
     )
 }
