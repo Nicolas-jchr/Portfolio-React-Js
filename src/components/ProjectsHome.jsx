98f408f644a45fada30ebbc21bbfa779b03b96b1
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-07-31 17:18:32 +0200

    Create card for projects

--- src/components/ProjectsHome.jsx
+++ src/components/ProjectsHome.jsx
@@ -1,15 +1,27 @@
-function ProjectsHome() {
-    
+import infoProjects from "../datas/projects";
 
-    return (
-        <>
-        <article>
-            <h2>
-                Mes projets réalisés
-            </h2>
-        </article>
-        </>
-    )
+const CardsProjects = ({ image, titre }) => {
+  return (
+    <div>
+      <img src={image} alt={`Logo du projet ${titre}`} />
+      <h3>{titre}</h3>
+    </div>
+  );
+};
+
+function ProjectsHome() {
+  return (
+    <article>
+      <h2>Mes projets réalisés</h2>
+      {infoProjects.map((project, index) => (
+        <CardsProjects 
+          key={index}
+          image={project.image}
+          titre={project.titre}
+        />
+      ))}
+    </article>
+  );
 }
 
 export default ProjectsHome;
